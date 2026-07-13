ed34c5722e07390b9fac9f9faba673a59ce28e3b
Ykirolos136 <kirolosmichelsobhy@gmail.com>
2026-07-13 09:04:24 -0400

    implementing general routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,32 +21,49 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    let isbn = req.params.isbn;
+    let bookFound = books[isbn];
+  
+    if (bookFound) {
+      return res.status(200).json(bookFound);
+    }
+    return res.status(404).json({message: "book doesn't exist"});
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let authorBook = Object.values(books).filter((book)=>book.author === author);
+  if (authorBook) {
+    return res.status(200).json(authorBook);
+  }
+  return res.status(404).json({message: "book doesn't exist"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let titleFound = Object.values(books).filter((book)=>book.title === title);
+  if(titleFound){
+    return res.status(200).json(titleFound);
+  }
+  return res.status(404).json({message: "book doesn't exist"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let book = books[isbn];
+
+    if(book){
+        return res.status(200).json(book.reviews);
+    }
+    return res.status(404).json({message:"book doesn't exist"});
 });
 
 module.exports.general = public_users;
